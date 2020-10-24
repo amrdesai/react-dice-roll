8ae803d166a9b04a83d783fbe3ecf40381e1ffaa
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-10-24 19:24:59 -0400

    Updates

---
+++ precache-manifest.731f6ad32be0ea5a84d358ee09a13b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515bb46b2e4edcf3680f075d85f34b6e",
+    "url": "/react-dice-roll/index.html"
+  },
+  {
+    "revision": "cdde159d8aa24371f3a5",
+    "url": "/react-dice-roll/static/css/main.12be30d4.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/react-dice-roll/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dice-roll/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdde159d8aa24371f3a5",
+    "url": "/react-dice-roll/static/js/main.9740e69d.chunk.js"
+  },
+  {
+    "revision": "090a3c20368c309703aa",
+    "url": "/react-dice-roll/static/js/runtime-main.01ce62a6.js"
+  }
+]);
